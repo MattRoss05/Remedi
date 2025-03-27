b5d824c199f833358bf72b7f10da95389bb55036
sdrossrva <sdrossrva@gmail.com>
2025-03-27 01:19:53 -0400

    creating Url Tests for Patient and Provider App

--- RemediBackend/users/test_user_urls.py
+++ RemediBackend/users/test_user_urls.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 # Create your tests here.
-class UserUrls(TestCase):
+class UserUrlTest(TestCase):
     def testRegister(self):
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
