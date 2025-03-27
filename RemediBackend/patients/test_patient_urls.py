edf9acdda5e46f14a4b09be585e4166c7c427bab
sdrossrva <sdrossrva@gmail.com>
2025-03-27 01:46:29 -0400

    re add patient test, apologies

--- RemediBackend/patients/test_patient_urls.py
+++ RemediBackend/patients/test_patient_urls.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+class PatientUrlTest(TestCase):
+    def testPatDash(self):
+        response = self.client.get(reverse('patient_dashboard'))
+        self.assertEqual(response.status_code, 200)
--- RemediBackend/providers/test_provider_urls.py
+++ RemediBackend/providers/test_provider_urls.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 # Create your tests here.
-class PatientUrlTest(TestCase):
-    def testDash(self):
+class ProviderUrlTest(TestCase):
+    def testProvDash(self):
         response = self.client.get(reverse('provider_dashboard'))
         self.assertEqual(response.status_code, 200)
