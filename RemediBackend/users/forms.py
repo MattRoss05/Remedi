e29fde27a7aaecf0760c9b4384bf038079e8891a
MattRoss05 <rossmj3@vcu.edu>
2025-03-27 02:09:41 -0400

    edited forms.py in users
    check if email already exists

--- RemediBackend/users/forms.py
+++ RemediBackend/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
+from email_validator import validate_email, EmailNotValidError, CHECK_DELIVERABILITY
 
 class ProviderRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -13,3 +13,15 @@ class ProviderRegisterForm(UserCreationForm):
             "password1",
             "password2",
         ]
+
+    #only need to add this custom method because Django will automatically fall it when form.is_valid() is called in views
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        #check if email already exists in the database
+        if CustomUser.objects.filter(email = email).exists():
+            raise forms.ValidationError('This email is already registered. Please use a different one.')
+
+        
+
+        return email
