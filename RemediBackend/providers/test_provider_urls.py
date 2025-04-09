7ad4dd3cc6d8a02fadcbf142ffb41f8f46645470
sdrossrva <sdrossrva@gmail.com>
2025-04-09 15:37:45 -0400

    Form tests for add patient and url test for add patient url

--- RemediBackend/providers/test_provider_urls.py
+++ RemediBackend/providers/test_provider_urls.py
@@ -3,9 +3,15 @@ from django.urls import reverse
 from users.models import CustomUser
 # Create your tests here
 class ProviderUrlTest(TestCase):
+    #create a user object to simulate a user
     def setUp(self):
         self.user = CustomUser.objects.create_user(username= 'test', password = 'testpass', user_type = 'provider')
     def testProvDash(self):
         self.client.login(username = 'test', password = 'testpass')
         response = self.client.get(reverse('provider_dashboard'))
+        self.assertEqual(response.status_code, 200)
+    # test for if the add patient url was reached
+    def testAddPatient(self):
+        self.client.login(username = 'test' , password = 'testpass', user_type = 'provider')
+        response = self.client.get(reverse('add_patient'))
         self.assertEqual(response.status_code, 200)
