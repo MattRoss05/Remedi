f644d35953a4f7ac985b7daa453733fda10d2a24
MattRoss05 <rossmj3@vcu.edu>
2025-04-10 12:44:52 -0400

    final touches for the sprint
    added titles to HTML files and added comments

--- RemediBackend/providers/views.py
+++ RemediBackend/providers/views.py
@@ -45,52 +45,72 @@ def add_patient(request):
     return render(request, 'providers/addpatient.html', {'patientForm': patientForm, 'userForm': userForm})
     
 def search_patient(request):
+    #if the user isnt authenticated as a provider, redirect to the welcome page
     if not request.user.is_authenticated or request.user.user_type == 'patient':
        return redirect('welcome')
+    #get the query from the user
     query = request.GET.get('q', '')
+    #if the query is not blank
     if query:
+        #get a list of matching patients based of either the first or last name, for now, you cannot use both
         patients = Patient.objects.filter(Q(first__icontains = query)|Q(last__icontains = query), provider = request.user.provider)
 
-                                          
+    #if the quuery is blank                                     
     else:
+        #get all patients related to the authenticated provider
         patients = Patient.objects.filter(provider = request.user.provider)
+    #render the html file with the acquired list of patients
     return render(request, 'providers/searchpatients.html', {'patients': patients, 'query': query})
 
 
 def delete_patient(request, patient_id):
+    #if the user is not authenitcated as a provider
     if not request.user.is_authenticated or request.user.user_type == 'patient':
+       #redirect to the welcome page.
        return redirect('welcome')
-    patient  = get_object_or_404(Patient, id = patient_id)
+    #obtain the patient to delete based off of id and the proider authenticated
+    patient  = get_object_or_404(Patient, id = patient_id, provider = request.user.provider)
 
+    #if the form has been submitted
     if request.method == "POST":
+        #delete the user associated with the patient, the patient model associated with the user will be by CASCADE
         patient.user.delete()
+        #redirect to search patients
         return redirect('search_patients')
     
     else:
+        #if the form has not been submitted, render the html file and pass the patient they can be displayed on the screen
         return render(request, 'providers/confirmdelete.html', {'patient': patient})
 
 
 def edit_patient(request, patient_id):
+    #if the user is not authenitcated as a provider
     if not request.user.is_authenticated or request.user.user_type == 'patient':
+       #redirect to the welcome page.
        return redirect('welcome')
-    
+     #obtain the patient to edit based off of id and the proider authenticated
     patient = get_object_or_404(Patient, id = patient_id, provider = request.user.provider)
 
-
+    # if the form has been submitted
     if request.method == "POST":
+        #intialize forms with the data provided from the user and the instance being changed
         editUserForm = EditPatientCustomUser(request.POST, instance = patient.user)
         editForm = EditPatientForm(request.POST, instance = patient)
+        #error prints from debugging
         print(editForm.errors)
         print(editUserForm.errors)
+        #if the forms are valid
         if editForm.is_valid() and editUserForm.is_valid():
+            # save them and redirect to search patients
             editForm.save()
             editUserForm.save()
             return redirect('search_patients')
         else:
-
+            #allows validation errors to be displayed
             return render(request, 'providers/editpatient.html', {'editForm': editForm, 'patient': patient, 'editUserForm': editUserForm})
         
     else:
+        #provide the forms to the user with existing information in thenm, allows for easier editing
         editForm = EditPatientForm(instance = patient)
         editUserForm = EditPatientCustomUser(instance = patient.user)
         return render(request, 'providers/editpatient.html', {'editForm': editForm, 'patient': patient, 'editUserForm': editUserForm})
@@ -98,18 +118,24 @@ def edit_patient(request, patient_id):
 
 
 def change_password(request, patient_id):
+    #if the user is not authenitcated as a provider
     if not request.user.is_authenticated or request.user.user_type == 'patient':
+       #redirect to welcome
        return redirect('welcome')
-    
+    #obtain the patient to edit based off of id and the proider authenticated
     patient = get_object_or_404(Patient, id = patient_id, provider = request.user.provider)
 
-
+    #if the form has been submitted
     if request.method == "POST":
+        #initialize th form with the data provided and the user to be edited
         changepasswordform = CustomPasswordChangeForm(user=patient.user,data= request.POST)
+        #if the form is valid
         if changepasswordform.is_valid():
+            #save the forms and redirect to editpatient page of the user just edited
             changepasswordform.save(user = patient.user)
             return redirect('edit_patient', patient_id = patient.id)
     else:
+        #otherwise provide blank forms with the user to be edited.
         changepasswordform = CustomPasswordChangeForm(user = patient.user)
         return render(request, 'providers/changepassword.html', {'patient':patient, 'changepasswordform':changepasswordform})
 
