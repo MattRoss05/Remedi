7a9c9674369de88870a2901f90231d2fff6966e5
tavsters <tavsters@gmail.com>
2025-04-24 17:35:51 -0400

    Patient dashboard basic html

--- RemediBackend/patients/views.py
+++ RemediBackend/patients/views.py
@@ -6,7 +6,23 @@ from django.contrib.auth.decorators import login_required
 def patient_dashboard(request):
     if request.user.is_authenticated:
         #render the patient dashbaord if logged in
-        return HttpResponse("Welcome to the patient dashboard!")
+        return render(request, 'patients/patientdashboard.html')
     else:
         return redirect('welcome')
+    
+def view_medications(request):
+    if request.user.is_authenticated:
+        #render the medication view if logged in as patient
+        return render(request, 'patients/viewmedications.html')
+    else:
+        return redirect('welcome')
+    
+
+def log_medications(request):
+    if request.user.is_authenticated:
+        #render the log medication view if logged in as patient
+        return render(request, 'patients/logmedications.html')
+    else:
+        return redirect('welcome')
+
 
